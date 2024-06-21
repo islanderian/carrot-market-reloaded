cef094563f1ae2631cb5ecda449513aa3b4250fa
islanderian <createdby.island@gmail.com>
2024-06-21 18:55:35 +0900

    #3.10 JIT

--- app/page.tsx
+++ app/page.tsx
@@ -68,6 +68,10 @@ export default function Home() {
           ))}
         </div>
       </div>
+      {/* JIT */}
+      <div className="flex items-center justify-center p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+        <div className="flex flex-col w-full max-w-screen-sm gap-3 p-5 bg-white shadow-lg rounded-sexy-name"></div>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,13 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      borderRadius: {
+        "sexy-name": "3323 px",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
