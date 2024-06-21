2c6527a9f6cf4ae4fe6d4d494eba5b33cb415f81
islanderian <createdby.island@gmail.com>
2024-06-21 18:01:34 +0900

    #3.8 Lists and Animation

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,28 @@ export default function Home() {
   return (
     <main>
       {/* Card Component */}
-      <div className="bg-gray-300 h-screen flex items-center justify-center p-5">
-        <div className="bg-white w-full shadow-lg p-5 rounded-2xl max-w-screen-sm">
-          <div className="flex justify-between items-center">
+      <div className="flex items-center justify-center p-5 bg-gray-300">
+        <div className="w-full max-w-screen-sm p-5 bg-white shadow-lg rounded-2xl">
+          <div className="flex items-center justify-between">
             <div className="flex flex-col">
-              <span className="text-gray-600 font-semibold -mb-1">
+              <span className="-mb-1 font-semibold text-gray-600">
                 In transit
               </span>
               <span className="text-4xl font-semibold">Coolblue</span>
             </div>
-            <div className="size-12 bg-orange-300 rounded-full" />
+            <div className="bg-orange-300 rounded-full size-12" />
           </div>
-          <div className="my-2 flex items-center gap-2">
+          <div className="flex items-center gap-2 my-2">
             <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
               Today
             </span>
             <span>9:30-10:30u</span>
           </div>
           <div className="relative">
-            <div className="bg-gray-200 w-full h-2 rounded-full absolute" />
-            <div className="bg-green-500 w-2/3 h-2 rounded-full absolute" />
+            <div className="absolute w-full h-2 bg-gray-200 rounded-full" />
+            <div className="absolute w-2/3 h-2 bg-green-500 rounded-full" />
           </div>
-          <div className="flex justify-between items-center mt-5 text-gray-600">
+          <div className="flex items-center justify-between mt-5 text-gray-600">
             <span>Expected</span>
             <span>Sorting center</span>
             <span>In transit</span>
@@ -32,20 +32,40 @@ export default function Home() {
         </div>
       </div>
       {/* Input */}
-      <div className="bg-gray-100 h-screen flex items-center justify-center p-5 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+      <div className="flex items-center justify-center p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
         <div className="bg-white  shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none has-[:invalid]:ring-red-100 ring ring-transparent transition-all">
           <input
-            className="w-full rounded-full h-12 bg-gray-200 pl-5 ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+            className="w-full h-12 pl-5 transition-shadow bg-gray-200 rounded-full ring ring-transparent focus:ring-green-500 focus:ring-offset-2 placeholder:drop-shadow invalid:focus:ring-red-500 peer"
             type="text"
             placeholder="Email address"
             required
           />
-          <span className="text-red-500 font-medium hidden peer-invalid:block">
+          <span className="hidden font-medium text-red-500 peer-invalid:block">
             Email is required.
           </span>
-          <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10">
+          <button className="py-2 font-medium text-white transition-transform bg-black rounded-full active:scale-90 md:px-10">
             Log in
           </button>
+        </div>
+      </div>
+      {/* List */}
+      <div className="flex items-center justify-center p-5 bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100">
+        <div className="flex flex-col w-full max-w-screen-sm gap-3 p-5 bg-white shadow-lg rounded-3xl">
+          {["Nico", "Me", "You", "Yourself", ""].map((person, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-5 last:border-0 last:pb-0"
+            >
+              <div className="bg-blue-400 rounded-full size-10" />
+              <span className="text-lg font-medium empty:w-24 empty:h-5 empty:bg-gray-300 empty:rounded-full empty:animate-pulse">
+                {person}
+              </span>
+              <div className="relative flex items-center justify-center text-white bg-red-500 rounded-full size-6 animate-bounce">
+                <span className="z-10">{index}</span>
+                <div className="absolute bg-red-500 rounded-full size-6 animate-ping"></div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </main>
